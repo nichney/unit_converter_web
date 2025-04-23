c20b17434902d5850c826a4877d6d066f3a6ba24
Osmolovsky K. <kirillosm09@gmail.com>
2025-04-24 01:00:34 +0300

    style: add styles

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Form, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
